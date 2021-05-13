6b3ea3a771c6951c9efa760710eade17f9d55747
丁文帅 <wenshuai@cecelive.com>
2021-05-13 18:12:31 +0800

    1、添加lombok开发工具包依赖。 2、get请求和post请求方式对比。 3、http header信息获取。

--- src/main/java/org/goodStudy/controller/TestController.java
+++ src/main/java/org/goodStudy/controller/TestController.java
@@ -1,7 +1,9 @@
 package org.goodStudy.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Author dingws
@@ -12,10 +14,48 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/test")
+@Slf4j
 public class TestController {
 
+    /**
+     * 本地浏览器输入：http://localhost:8088/test/hello
+     * @return
+     */
     @RequestMapping("/hello")
     public String getTestInfo(){
+        log.info("收到请求");
         return "hello spring boot";
+    }
+
+    /**
+     * get请求方式
+     * @param param
+     * @return
+     */
+    @RequestMapping("/getTypeReq")
+    public String getTypeReq(@RequestParam String param){
+        log.info("收到get请求, param =" + param);
+        return "收到get请求";
+    }
+
+    /**
+     * post请求方式
+     * @param param
+     * @return
+     */
+    @RequestMapping("/postTypeReq")
+    public String postTypeReq(@RequestBody String param){
+        log.info("收到post请求, param =" + param);
+        return "收到post请求";
+    }
+
+    /**
+     * 获取http header 信息
+     * @return
+     */
+    @RequestMapping("/getHeaderInfo")
+    public String getHeaderInfo(@RequestHeader HttpHeaders httpHeaders){
+        log.info("收到请求, http header info = " + httpHeaders);
+        return "收到请求, http header info = " + httpHeaders;
     }
 }
