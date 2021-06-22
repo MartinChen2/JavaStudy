cd4150a47c3632706f9939a89557dac5730444ff
丁文帅 <wenshuai@cecelive.com>
2021-06-22 14:47:45 +0800

    1、添加加载资源文件工具类。

---
+++ src/main/java/org/goodStudy/Util/LoadResourceUtil.java
@@ -0,0 +1,15 @@
+package org.goodStudy.Util;
+
+import java.io.InputStream;
+
+public class LoadResourceUtil {
+    void load(){
+        InputStream inputStream = LoadResourceUtil.class.getResourceAsStream("test.txt");
+        System.out.println("inputStream = " +  inputStream);
+    }
+
+    public static void main(String[] args) {
+        LoadResourceUtil loadResourceUtil = new LoadResourceUtil();
+        loadResourceUtil.load();
+    }
+}
